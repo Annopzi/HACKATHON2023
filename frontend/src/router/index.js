f8b9397a319778614232310d3244c6298f71d344
Annopzi <6331305031@lamduan.mfu.ac.th>
2023-07-30 01:59:41 +0700

    setup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import MainDefault from '../layout/MainDefault.vue'
 
 Vue.use(VueRouter)
 
@@ -8,15 +10,24 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: MainDefault,
+    children:[
+      {
+        path:'',
+        component: HomeView
+      }
+    ]
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: MainDefault,
+    children:[
+      {
+        path:'',
+        component: AboutView
+      }
+    ]
   }
 ]
 
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <v-btn to="/">
+    back
+  </v-btn>
   </div>
 </template>
